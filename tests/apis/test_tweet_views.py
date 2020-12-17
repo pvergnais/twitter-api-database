4536bff93ce32156ed68666cabee364cdea0807a
pvergnais <74971671+pvergnais@users.noreply.github.com>
2020-12-17 18:07:34 +0100

    added a postgres db and travis

--- tests/apis/test_tweet_views.py
+++ tests/apis/test_tweet_views.py
@@ -1,26 +1,32 @@
 from flask_testing import TestCase
 from app import create_app
 from app.models import Tweet
-from app.db import tweet_repository
+from app import db
 
 class TestTweetViews(TestCase):
     def create_app(self):
         app = create_app()
         app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = f"{app.config['SQLALCHEMY_DATABASE_URI']}_test"
         return app
 
     def setUp(self):
-        tweet_repository.clear() # Upgrade the TweetRepository.__clear() method to public!
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
     def test_read_many_tweets(self):
         first_tweet = Tweet('First tweet')
-        tweet_repository.add(first_tweet)
+        db.session.add(first_tweet)
         second_tweet = Tweet('Second tweet')
-        tweet_repository.add(second_tweet)
+        db.session.add(second_tweet)
+        db.session.commit()
 
         response = self.client.get('/tweets')
         response_tweets = response.json
-        print(response_tweets)
+        # print(response_tweets)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response_tweets), 2)
@@ -37,10 +43,11 @@ class TestTweetViews(TestCase):
 
     def test_read_one_tweet(self):
         first_tweet = Tweet('First tweet')
-        tweet_repository.add(first_tweet)
+        db.session.add(first_tweet)
+        db.session.commit()
         response = self.client.get('/tweets/1')
         response_tweet = response.json
-        print(response_tweet)
+        # print(response_tweet)
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response_tweet['id'], 1)
@@ -51,6 +58,7 @@ class TestTweetViews(TestCase):
         response = self.client.post('/tweets', json={'text': 'New tweet!'})
         created_tweet = response.json
 
+        # print(created_tweet)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(created_tweet['id'], 1)
         self.assertEqual(created_tweet['text'], 'New tweet!')
@@ -58,24 +66,26 @@ class TestTweetViews(TestCase):
 
     def test_update_one_tweet(self):
         tweet_to_update = Tweet('Tweet to update')
-        tweet_repository.add(tweet_to_update)
+        db.session.add(tweet_to_update)
+        db.session.commit()
         response = self.client.patch('/tweets/1', json={'text': 'New text'})
 
         self.assertEqual(response.status_code, 204)
 
         # We use direct access to database to validate our operation
         # Database return Tweet instance, not json converted to a dict
-        updated_tweet = tweet_repository.get(1)
+        updated_tweet = db.session.query(Tweet).get(1)
         self.assertEqual(updated_tweet.id, 1)
         self.assertEqual(updated_tweet.text, 'New text')
         self.assertIsNotNone(updated_tweet.created_at)
 
     def test_delete_one_tweet(self):
         tweet_to_delete = Tweet('A tweet')
-        tweet_repository.add(tweet_to_delete)
+        db.session.add(tweet_to_delete)
+        db.session.commit()
         response = self.client.delete('/tweets/1')
 
         self.assertEqual(response.status_code, 204)
 
         # We use direct access to database to validate our operation
-        self.assertIsNone(tweet_repository.get(1))
+        self.assertIsNone(db.session.query(Tweet).get(1))
